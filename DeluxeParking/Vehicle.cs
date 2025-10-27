ec0b00d01a29f99fbb41d75da909396502e63cac
antonhag <anton.hagstrom@live.com>
2025-10-27 10:23:33 +0100

    Made Vehicle class abstract

--- DeluxeParking/Vehicle.cs
+++ DeluxeParking/Vehicle.cs
@@ -1,22 +1,17 @@
 namespace DeluxeParking;
 
-public class Vehicle
+public abstract class Vehicle
 {
     public string LicensePlateNumber { get; set; }
     public string VehicleColor { get; set; }
     public double VehicleSize { get; set; }
 
-    public Vehicle(string licensePlateNumber, string vehicleColor, double vehicleSize)
+    protected Vehicle(string licensePlateNumber, string vehicleColor, double vehicleSize)
     {
         LicensePlateNumber = licensePlateNumber;
         VehicleColor = vehicleColor;
         VehicleSize = vehicleSize;
     }
 
-    public virtual string PrintVehicle()
-    {
-        string vehicleInfo = "";
-        
-        return vehicleInfo;
-    }
+    public abstract string PrintVehicle();
 }
